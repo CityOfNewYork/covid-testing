d7e719650fdc011c03a46018f6191c63dfe39b48
timkeane <t.d.keane@gmail.com>
2020-05-11 09:53:07 -0400

    better details html

--- src/js/decorations.js
+++ src/js/decorations.js
@@ -8,6 +8,20 @@ export default [{
       '</span></b><br><span class="srch-lbl-sm">' + this.getFullAddress() + '</span>'
     )
   },
+  html() {
+    return $('<div class="facility"></div>')
+      .addClass(this.cssClass())
+      .append(this.distanceHtml())
+      .append(this.nameHtml())
+      .append(this.distanceHtml(true))
+      .append(this.addressHtml())
+      .append(this.details())
+      .append(this.mapButton())
+      .append(this.directionsButton())
+      .data('feature', this)
+      .mouseover($.proxy(this.handleOver, this))
+      .mouseout($.proxy(this.handleOut, this))
+  },
   getFullAddress() {
     return `${this.get('Address')}, ${this.get('Borough')}, NY`
   },
@@ -23,12 +37,16 @@ export default [{
   getTip() {
     return this.get('search_label')
   },
-  detailsCollapsible() {
+  details() {
     const walkIn = this.get('Walk-in?') === 'Y' ? 'Yes' : 'No'
     const nycha = this.get('NYCHA priority site?') === 'Y' ? 'Yes' : 'No'
-    return $('<div class="detail"></div>')
-      .append(`<strong>Walk in facility: </strong> ${walkIn}`)
-      .append(`<strong>NYCHA priority site: </strong> ${nycha}`)
-      .append(`<strong>Prioritization criteria:<br></strong> ${this.get('Prioritization criteria')}`)
+    const criterior =  this.get('Prioritization criteria')
+    const details = $('<div class="detail"></div>')
+      .append(`<div><strong>Walk in facility: </strong> ${walkIn}</div>`)
+      .append(`<div><strong>NYCHA priority site: </strong> ${nycha}</div>`)
+    if (criterior) {
+      details.append(`<div><strong>Prioritization criteria:<br></strong> ${criterior}</div>`)
+    }
+    return details
   }
 }]
