634882be013068223c5ce5978ad9e8931d25d72b
Tim Keane <t.d.keane@gmail.com>
2022-07-01 10:29:17 -0400

    prod data

--- src/js/urls.js
+++ src/js/urls.js
@@ -8,6 +8,6 @@ const today_dd = now.getDate().toString().padStart(2, '0')
 const today = `${today_yyyy}-${today_mm}-${today_dd}`
 
 export default {
-  FACILITY_URL: `https://services6.arcgis.com/yG5s3afENB5iO9fj/arcgis/rest/services/COVIDTestingSites_TEST_VIEW/FeatureServer/0/query?f=geojson&cacheHint=true&outSR=3857&outFields=FacilityName,FacilityType,ADACompliant,Address,Borough,ZipCode,Phone,Website,WalkInsWelcome,MinimumAge,AppointmentsAvailable,AdditionalInfo&where=StartDate%20%3C=%20%27${today}%27%20AND%20EndDate%20%3E=%20%27${today}%27&${cacheBust}`,
+  FACILITY_URL: `https://services6.arcgis.com/yG5s3afENB5iO9fj/arcgis/rest/services/COVIDTestingSites_PROD_VIEW/FeatureServer/0/query?f=geojson&cacheHint=true&outSR=3857&outFields=FacilityName,FacilityType,ADACompliant,Address,Borough,ZipCode,Phone,Website,WalkInsWelcome,MinimumAge,AppointmentsAvailable,AdditionalInfo&where=StartDate%20%3C=%20%27${today}%27%20AND%20EndDate%20%3E=%20%27${today}%27&${cacheBust}`,
   GEOCLIENT_URL: 'https://maps.nyc.gov/geoclient/v1/search.json?app_key=74DF5DB1D7320A9A2&app_id=nyc-lib-example'
 }
