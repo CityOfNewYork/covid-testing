0ca11f4a7bb42f2f0501fecbb94c1fcf2dab9822
Tim Keane <t.d.keane@gmail.com>
2022-07-13 08:09:59 -0400

    index test requires olms mock

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import App from './App'
 import urls from './urls'
 import olms from 'ol-mapbox-style'
+import $ from 'jquery'
 
 olms(
   $('<div class="tab tab-0 active" aria-labelledby="tab-btn-0" role="tabpanel" aria-hidden="false" tabindex="1000"></div>').get(0),
